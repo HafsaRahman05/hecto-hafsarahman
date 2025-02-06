31815aa2e92d87e0f6ca2d78c8a52015e74e288e
HafsaRahman05 <hafsarahman805@gmail.com>
2025-02-07 04:08:59 +0500

    index.tsx

--- pages/navbar/index.tsx
+++ pages/navbar/index.tsx
@@ -67,10 +67,11 @@ function Navbar() {
       >
         ↑
       </button>
-<header className="font-sans border-b relative">
-<div className=" bg-[#7E33E0] text-white py-2">
-<div className="container mx-auto flex justify-between items-center text-sm px-6 lg:px-10">
-<div className="flex items-center gap-6">
+<div className="2xl:w-auto h-[44px] w-full bg-[#7E33E0] text-[#f1f1f1] flex justify-center items-center">
+
+
+<div className="container mx-auto flex justify-between items-center text-sm ">
+<div className="flex items-center gap-3">
 <div className="flex items-center gap-2">
 <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
 <g id="Mail">
@@ -92,7 +93,7 @@ function Navbar() {
 <p>(1234) 567890</p>
 </div>
 </div>
-<div className="flex items-center gap-6">
+<div className="flex items-center gap-3">
 <label htmlFor="language-select" className="sr-only">Language</label>
 <select id="language-select" className=" bg-[#7E33E0] hover:bg-purple-400 px-2 py-1 rounded">
 <option>English</option>
@@ -136,23 +137,24 @@ function Navbar() {
 </p>
 </a>
 </div>
-<svg stroke="currentColor" fill="none" stroke-width="1.5" viewBox="0 0 24 24" aria-hidden="true" className="block sm:hidden" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+{/* <svg stroke="currentColor" fill="none" stroke-width="1.5" viewBox="0 0 24 24" aria-hidden="true" className="block sm:hidden" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z">
 </path>
 </svg>
 <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" className="block sm:hidden" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
 <path d="M923 283.6a260.04 260.04 0 0 0-56.9-82.8 264.4 264.4 0 0 0-84-55.5A265.34 265.34 0 0 0 679.7 125c-49.3 0-97.4 13.5-139.2 39-10 6.1-19.5 12.8-28.5 20.1-9-7.3-18.5-14-28.5-20.1-41.8-25.5-89.9-39-139.2-39-35.5 0-69.9 6.8-102.4 20.3-31.4 13-59.7 31.7-84 55.5a258.44 258.44 0 0 0-56.9 82.8c-13.9 32.3-21 66.6-21 101.9 0 33.3 6.8 68 20.3 103.3 11.3 29.5 27.5 60.1 48.2 91 32.8 48.9 77.9 99.9 133.9 151.6 92.8 85.7 184.7 144.9 188.6 147.3l23.7 15.2c10.5 6.7 24 6.7 34.5 0l23.7-15.2c3.9-2.5 95.7-61.6 188.6-147.3 56-51.7 101.1-102.7 133.9-151.6 20.7-30.9 37-61.5 48.2-91 13.5-35.3 20.3-70 20.3-103.3.1-35.3-7-69.6-20.9-101.9zM512 814.8S156 586.7 156 385.5C156 283.6 240.3 201 344.3 201c73.1 0 136.5 40.8 167.7 100.4C543.2 241.8 606.6 201 679.7 201c104 0 188.3 82.6 188.3 184.5 0 201.2-356 429.3-356 429.3z">
 </path>
-</svg>
+</svg> */}
 </div>
 </div>
+
+
 </div>
-</header>
 
 
 
       {/* Navbar 
-      <div className="2xl:w-auto h-[44px] w-full bg-[#7E33E0] text-[#f1f1f1] flex justify-center items-center">
+      <d_iv className="2xl:w-auto h-[44px] w-full bg-[#7E33E0] text-[#f1f1f1] flex justify-center items-center">
         <div className="w-full md:w-[80%] 2xl:w-[1177px] flex flex-col justify-center lg:flex-row md:justify-between items-center overflow-hidden">
           {/* Left section 
           <div className="w-[30%] lg:w-[380px] flex items-center justify-center lg:justify-start">
